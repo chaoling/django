2ad1f751eaf6dc6000abfd4cefead1383c4d41ef
Chao Ling <ling.chao@gmail.com>
2022-08-07 01:48:36 +0000

    added template

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path("", views.index, name="index")
+     path("", views.index, name="index"),
+     path("bob", views.bob, name="bob"),
+     path("david", views.david, name="david"),
+     path("<str:name>", views.greet, name="greet")
  ]
--- hello/views.py
+++ hello/views.py
@@ -4,4 +4,17 @@ from django.http import HttpResponse
  # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    #return HttpResponse("<h1 style=\"color:blue\">Hello, world!</h1>")
+    return render(request, "hello/index.html")
+
+def bob(request):
+    return HttpResponse("Hello, Bob!")
+    
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    #return HttpResponse(f"Hello, {name}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
